33a5d2aeb5763583782a221c984ffd7fff987817
Ekas <edvis.kulka@gmail.com>
2021-11-24 14:16:15 +0200

    Add Products Layout in Blazor WebAssembly App
    Adds a /products page to our blazor webassembly app with placeholder values

--- TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
+++ TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
@@ -44,7 +44,7 @@ namespace TRMDesktopUI.ViewModels
                 SelectedUserName = value.Email;
                 UserRoles = new BindingList<string>(value.Roles.Select(x => x.Value).ToList());
                 // TODO - Pull this out into a method/event
-                LoadRoles().Wait();
+                LoadRoles();
                 NotifyOfPropertyChange(() => SelectedUser);
             }
         }
